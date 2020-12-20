518a1a0651a7dbb64ee50d9a980a183e07862e64
Jason Fu <69946005+jlfud@users.noreply.github.com>
2020-12-19 19:38:20 -0800

    started form, need to build backend to process

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-//https://tinyurl.com/gabiscrochet
+//https://tinyurl.com/gabicrochet
 //use paypal: https://smallbusiness.chron.com/make-payment-button-set-price-paypal-49583.html
+
+function cart(){
+
+}
