f0f565af1e07cd273cfbf61a438e5fc37bf69cec
Jason Fu <69946005+jlfud@users.noreply.github.com>
2020-12-24 11:04:59 -0800

    ug

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 //https://tinyurl.com/gabicrochet
 //use paypal: https://smallbusiness.chron.com/make-payment-button-set-price-paypal-49583.html
-
-function cart(){
-
+var globalItems = [];
+localStorage.setItem("array", JSON.stringify(globalItems))
+function checkForItems(){
+    sessionStorage.setItem("test", "test again")
+    if(document.getElementById("bandana-color").value != null){
+        var bandana = {type: "bandana", color1: document.getElementById("bandana-color").value, color2: null, size: null};
+        items = JSON.parse(localStorage.getItem("array"))
+        items.push(bandana)
+        localStorage.setItem("array", JSON.stringify(items))
+    }
+}
+function printItems(items){
+    console.log(items.length)
+    for(i = 0; i < items.length; i++){
+        console.log(items[i].type + ", " + items[i].color1)
+    }
 }
