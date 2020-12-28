5eab7d60ed7d093334c254c4001a46daaa9ed669
Jason Fu <69946005+jlfud@users.noreply.github.com>
2020-12-28 10:43:11 -0800

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//https://tinyurl.com/gabicrochet
+//https://tinyurl.com/gabiscrochet
 //use paypal: https://smallbusiness.chron.com/make-payment-button-set-price-paypal-49583.html
 var globalItems = [];
 localStorage.setItem("array", JSON.stringify(globalItems))
